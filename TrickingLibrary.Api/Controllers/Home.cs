beb9e074e6cabf3283df9a079db01e02ebf16e71
taha7 <taha.mohamed@iken.tech>
2020-11-16 14:29:05 +0200

    init

---
+++ TrickingLibrary.Api/Controllers/Home.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello world!";
+        }
+    }
+}
